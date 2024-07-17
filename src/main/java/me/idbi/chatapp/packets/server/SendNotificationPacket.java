fa12d67c9cda7483db6d572043d3fdb047a43b33
kovacs-balazs <kbalu2006@gmail.com>
2024-07-17 12:46:07 +0200

    Room manage commands

---
+++ src/main/java/me/idbi/chatapp/packets/server/SendNotificationPacket.java
@@ -0,0 +1,20 @@
+package me.idbi.chatapp.packets.server;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import me.idbi.chatapp.notifications.Notification;
+import me.idbi.chatapp.notifications.Notifications;
+import me.idbi.chatapp.packets.ServerPacket;
+
+@Getter
+public class SendNotificationPacket extends ServerPacket {
+    private Notification notification;
+
+    public SendNotificationPacket(Notifications notifications) {
+        this.notification = notifications.getNotification();
+    }
+
+    public SendNotificationPacket(Notification notification) {
+        this.notification = notification;
+    }
+}
